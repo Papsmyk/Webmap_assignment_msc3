24998f19af194f504f793b3c3f7ed1e7be2d729d
Mike Passiany <38376135+Papsmyk@users.noreply.github.com>
2024-02-21 02:48:26 +0300

    Add files via upload

---
+++ data/pointsuon.js
@@ -0,0 +1,11 @@
+var pointJSON = {
+"type": "FeatureCollection",
+"name": "points",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "American Wing" }, "geometry": { "type": "Point", "coordinates": [ 36.817465771508274, -1.278596860626853 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "Chiromo Campus" }, "geometry": { "type": "Point", "coordinates": [ 36.804881984692571, -1.273271823947574 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "School of Law" }, "geometry": { "type": "Point", "coordinates": [ 36.818637719058039, -1.268635306453786 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "KCA University" }, "geometry": { "type": "Point", "coordinates": [ 36.859525870368735, -1.253438442835721 ] } }
+]
+}
